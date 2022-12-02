bda588ca998130ebf494152e4d07273601b31b97
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-01 19:47:05 -0500

    Create Movies.swift
    Created Data Model for Movie

---
+++ IOS-Assignment4/Movies.swift
@@ -0,0 +1,18 @@
+//
+//  Movies.swift
+//  IOS-Assignment4
+//
+//  Created by Hannan Max on 2022-12-01.
+//
+
+import Foundation
+
+// Data Model : Movie
+class Movies: NSObject{
+    
+    var MovieName: String
+    init(MovieName: String){
+        self.MovieName = MovieName
+    }
+    
+}
